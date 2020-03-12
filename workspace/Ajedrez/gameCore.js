140ecb2e07946b71fd24995d2c8a2fbbe122d546
Joel <joelserranoservera12@gmail.com>
2020-03-12 12:26:15 +0100

    Falta movimiento torre lateral tanto derecha como izquierda

--- workspace/Ajedrez/gameCore.js
+++ workspace/Ajedrez/gameCore.js
@@ -1,6 +1,5 @@
 //Variables generales para guardar objetos
 let arrayPiezas = new Array();
-
 let ultimaPieza;
 
 //Clases para la creacion de los objetos
@@ -22,6 +21,10 @@ function Peon(color, pos,first) {
 
 Peon.prototype.toString = function() {
   return this.name + " " + this.color;
+};
+
+Peon.prototype.getColor = function() {
+  return this.color;
 };
 
 Peon.prototype.sombrear = function() {
@@ -126,8 +129,9 @@ Peon.prototype.movimiento = function(pos) {
     esteObjeto.sombrear()
   })
   $("." + this.pos).append(img);
-
+  finTurno(ultimaPieza)
   ultimaPieza = null;
+  
 };
 
 //Clase caballo
@@ -146,7 +150,9 @@ function Caballo(color, pos) {
 Caballo.prototype.toString = function() {
   return this.name + " " + this.color;
 };
-
+Caballo.prototype.getColor = function() {
+  return this.color;
+};
 Caballo.prototype.sombrear=function () { 
 borrarSombreado()
   let sombreado=new Array()
@@ -159,13 +165,13 @@ borrarSombreado()
     sombreado.push(parseInt(this.pos)+12)
     sombreado.push(parseInt(this.pos)+8)
 
-  let estaPieza=this
+  ultimaPieza=this
   sombreado.map(sombra=>{
     if($('.'+sombra).find('img').length<1){
       $("." +sombra).parent().css("background-color", "red");
       $("."+sombra).click(function(){
         borrarSombreado()
-        estaPieza.movimiento(sombra)
+        ultimaPieza.movimiento(sombra)
         $('div').each(function(){
           $(this).off()
         })
@@ -176,7 +182,7 @@ borrarSombreado()
  }
 
 Caballo.prototype.movimiento = function(pos) {
-  let esteObjeto=this;
+  ultimaPieza=this;
   $("." + this.pos)
     .find("img")
     .remove();
@@ -185,12 +191,11 @@ Caballo.prototype.movimiento = function(pos) {
   img.src = this.img;
   img.width = 60;
   $(img).click(function(){
-    esteObjeto.sombrear()
+    ultimaPieza.sombrear()
   })
   $("." + this.pos).append(img);
-
+  finTurno(ultimaPieza)
   ultimaPieza = null;
-
 };
 
 //Clase alfil
@@ -209,11 +214,14 @@ function Alfil(color, pos) {
 Alfil.prototype.toString = function() {
   return this.name + " " + this.color;
 };
+Alfil.prototype.getColor = function() {
+  return this.color;
+};
 Alfil.prototype.sombrear=function () { 
   borrarSombreado()
   let sombreado=new Array()
   let calculo=0;
-
+  ultimaPieza=this
   //Movimiento en diagonal arriba derecha
   let imagen=false;
   let i=1;
@@ -240,14 +248,14 @@ Alfil.prototype.sombrear=function () {
         sombreado.push(parseInt(calculo))
       }else{
         imagen=true;
-        console.log("hay una imagen en medio")
+        //console.log("hay una imagen en medio")
       }
     }else if(calculo.toString()[1]=="8"){
 
       if($("#"+calculo).find("img").length<1){
         sombreado.push(parseInt(calculo))
       }else{
-        console.log("hay una imagen en medio")
+        //console.log("hay una imagen en medio")
       }
       imagen=true;
     }
@@ -260,20 +268,22 @@ Alfil.prototype.sombrear=function () {
   i=0;
   do{
     calculo=parseInt(this.pos)+parseInt(i+""+9-i)
-
+    console.log(calculo)
+    if(calculo.toString()[1]=="9")imagen=true
     if(calculo.toString()[1]!="8"){
       if($("#"+calculo).find("img").length<1){
         sombreado.push(parseInt(calculo))
       }else{
         imagen=true;
-        console.log("hay una imagen en medio")
+        //console.log("hay una imagen en medio")
       }
     }else if(calculo.toString()[1]=="8"){
 
       if($("#"+calculo).find("img").length<1){
         sombreado.push(parseInt(calculo))
       }else{
-        console.log("hay una imagen en medio")
+        //AKA MATAR
+        //console.log("hay una imagen en medio")
       }
       imagen=true;
     }
@@ -286,34 +296,36 @@ Alfil.prototype.sombrear=function () {
    i=1;
   do{
     calculo=parseInt(this.pos)+parseInt(i+""+i)
-    console.log(calculo)
+    //console.log(calculo)
     if($("#"+calculo).find("img").length<1){
       sombreado.push(calculo)
     }else{
+      //AKA MATAR
       imagen=true;
-      console.log("hay una imagen en medio")
+      //console.log("hay una imagen en medio")
     }
     i++
   }while(i<8 && !imagen)
 
-  let estaPieza=this
+  
   sombreado.map(sombra=>{
     if($('.'+sombra).find('img').length<1){
       $("." +sombra).parent().css("background-color", "red");
       $("."+sombra).click(function(){
         borrarSombreado()
-        estaPieza.movimiento(sombra)
+        ultimaPieza.movimiento(sombra)
         $('div').each(function(){
           $(this).off()
         })
       })
-      console.log("hi ha una imatge")
+     // console.log("hi ha una imatge")
     }
   })
  }
+
 Alfil.prototype.movimiento = function(pos) {
   console.log("ha entrado con la pos: "+pos)
-  let esteObjeto=this;
+  ultimaPieza=this;
   $("." + this.pos)
     .find("img")
     .remove();
@@ -322,12 +334,10 @@ Alfil.prototype.movimiento = function(pos) {
   img.src = this.img;
   img.width = 60;
   $(img).click(function(){
-    esteObjeto.sombrear()
+    ultimaPieza.sombrear()
   })
   $("." + this.pos).append(img);
-
-  ultimaPieza = null;
-
+  finTurno(ultimaPieza)
 };
 
 
@@ -347,8 +357,52 @@ function Rei(color, pos) {
 Rei.prototype.toString = function() {
   return this.name + " " + this.color;
 };
-Rei.prototype.movimiento = function() {
-  console.log("movimiento del rei");
+Rei.prototype.getColor = function() {
+  return this.color;
+};
+Rei.prototype.movimiento = function(pos) {
+  ultimaPieza=this;
+  $("." + this.pos)
+    .find("img")
+    .remove();
+  this.pos = pos;
+  let img = document.createElement("img");
+  img.src = this.img;
+  img.width = 60;
+  $(img).click(function(){
+    ultimaPieza.sombrear()
+  })
+  $("." + this.pos).append(img);
+  finTurno(ultimaPieza)
+  ultimaPieza = null;
+};
+Rei.prototype.sombrear = function() {
+  borrarSombreado()
+  ultimaPieza=this
+  let pos=parseInt(this.pos)
+  let sombreado=new Array()
+  sombreado.push(pos+11)
+  sombreado.push(pos+10)
+  sombreado.push(pos+9)
+  sombreado.push(pos+1)
+  sombreado.push(pos-1)
+  sombreado.push(pos-9)
+  sombreado.push(pos-10)
+  sombreado.push(pos-11)
+
+  sombreado.map(sombra=>{
+    if($('.'+sombra).find('img').length<1){
+      $("." +sombra).parent().css("background-color", "red");
+      $("."+sombra).click(function(){
+        borrarSombreado()
+        ultimaPieza.movimiento(sombra)
+        $('div').each(function(){
+          $(this).off()
+        })
+      })
+      console.log("hi ha una imatge")
+    }
+  })
 };
 
 function Reina(color, pos) {
@@ -366,8 +420,15 @@ function Reina(color, pos) {
 Reina.prototype.toString = function() {
   return this.name + " " + this.color;
 };
+Reina.prototype.getColor = function() {
+  return this.color;
+};
 Reina.prototype.movimiento = function() {
   console.log("movimiento de la reina");
+};
+
+Reina.prototype.sombrear = function() {
+  console.log("sombrear de la reina");
 };
 
 function Torre(color, pos) {
@@ -385,11 +446,118 @@ function Torre(color, pos) {
 Torre.prototype.toString = function() {
   return this.name + " " + this.color;
 };
-Torre.prototype.movimiento = function() {
-  console.log("movimiento de la torre");
+Torre.prototype.getColor = function() {
+  return this.color;
+};
+Torre.prototype.movimiento = function(pos) {
+  ultimaPieza=this;
+  $("." + this.pos)
+    .find("img")
+    .remove();
+  this.pos = pos;
+  let img = document.createElement("img");
+  img.src = this.img;
+  img.width = 60;
+  $(img).click(function(){
+    ultimaPieza.sombrear()
+  })
+  $("." + this.pos).append(img);
+  finTurno(ultimaPieza)
+  ultimaPieza = null;
+};
+
+Torre.prototype.sombrear = function() {
+  borrarSombreado();
+  ultimaPieza=this;
+  
+  let sombreado=new Array();
+  let pos=parseInt(this.pos);
+  let i=1;
+  let calculo=0;
+  let imagen=false;
+  
+  //Movimiento hacia abajo
+  do{
+    calculo=pos+parseInt(i+"0")
+    
+    if(calculo.toString().length<3 && calculo<90){
+      if($("#"+calculo).find("img").length<1){
+        sombreado.push(calculo);
+      }else{
+        imagen=true;
+      }
+    }
+    i++
+  }while(i<9 && !imagen)
+
+  //movimiento hacia arriba
+  imagen=false;
+  i=1;
+  do{
+
+   calculo=pos-parseInt(i+"0")
+
+    if(calculo!=this.pos){
+      if(calculo.toString().length>1 && calculo>9){
+        if($("#"+calculo).find("img").length<1){
+          sombreado.push(calculo);
+        }else{
+          imagen=true;
+        }
+      }
+    }
+    calculo=this.pos[0]+i;
+    sombreado.push(calculo)
+    i++;
+  }while(i<9 && !imagen)
+  //console.log(sombreado)
+
+  //Movimiento lateral WIP
+  imagen=false;
+  i=1;
+  do{
+    calculo=ultimaPieza.pos.toString()[0]+i;
+    if(calculo!=this.pos){
+      console.log("entro con la pos: "+calculo)
+      if($("#"+calculo).find("img").length<1){ 
+        sombreado.push(calculo)
+      }
+      else{imagen=true}
+    }
+    i++;
+  }while(i<9 && !imagen)
+
+  sombreado.map(sombra=>{
+    if($('.'+sombra).find('img').length<1){
+      $("." +sombra).parent().css("background-color", "red");
+      $("."+sombra).click(function(){
+        borrarSombreado()
+        ultimaPieza.movimiento(sombra)
+        $('div').each(function(){
+          $(this).off()
+        })
+      })
+      console.log("hi ha una imatge")
+    }
+  })
+
 };
 
 //Funciones generales para el funcionamiento del ajedrez
+
+function finTurno(obj){
+  
+  console.log(ultimaPieza.getColor());
+  $("img").each(function () { 
+   $(this).off()
+   })
+   if(ultimaPieza.getColor()=="negro"){
+    recargarPiezas("blanco")
+   }else{
+    recargarPiezas("negro")
+   }
+   ultimaPieza=null;
+}
 
 function borrarSombreado(){
   $('td').each(function(){
@@ -476,6 +644,26 @@ function crearPiezas() {
   });
 }
 
+function recargarPiezas(color) {
+  $("img").each(function (){
+    $(this).remove()
+  })
+  arrayPiezas.map(pieza => {
+    let img = document.createElement("img");
+    img.src = pieza.img;
+    if(pieza.getColor()==color){
+      $(img).click(function() {
+        pieza.sombrear();
+        
+      });
+    }
+      img.width = 60;
+      img.alt=pieza.color
+      $("." + pieza.pos).append(img);
+    
+    
+  });
+}
 //GAMECORE
 crearTablero();
 crearPiezas();
